d610f3149e23d0229bf8e9cbcb03efc7b22185d5
Osmany <osmarguet@gmail.com>
2025-08-28 13:11:00 -0400

    App cente update

--- app/(tabs)/app_center.tsx
+++ app/(tabs)/app_center.tsx
@@ -1,25 +1,25 @@
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View } from 'react-native';
 
 export default function AppCenterScreen() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container} edges={['top']}>
-        <ScrollView style={styles.scrollView}>
-          <Text>
-            Hello world
-          </Text>
-        </ScrollView>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ThemedView style={styles.container}>
+      <View style={styles.container}>
+        <ThemedText>Hi</ThemedText>
+      </View>
+      <View style={styles.container}>
+        <ThemedText>Hi</ThemedText>
+      </View>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 0,
+    paddingTop: 5,
   },
   scrollView: {
     backgroundColor: 'rgba(50,50,50,1)',
@@ -28,5 +28,5 @@ const styles = StyleSheet.create({
     fontSize: 42,
     padding: 12,
     color: 'rgba(255, 255, 255, 1)'
-  },
+  }
 });
