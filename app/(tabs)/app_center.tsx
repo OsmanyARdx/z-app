ae2323600ab26b01584a054415ea138c275fd55f
JPR <jonatanp613@gmail.com>
2026-01-07 13:54:47 -0500

    PDF Viewer Done for android on WebHosted PDF Files

--- app/(tabs)/app_center.tsx
+++ app/(tabs)/app_center.tsx
@@ -1,67 +1,72 @@
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { navigate } from 'expo-router/build/global-state/routing';
-import React from 'react';
-import { Button, SectionList, StyleSheet, Switch, View } from 'react-native';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { router } from "expo-router";
+import React from "react";
+import { Button, SectionList, StyleSheet, View } from "react-native";
+
+
+
+
 
 export default function AppCenterScreen() {
+  
 
   const DATA = [
     {
-      title: 'Common Functions',
-      data: ['Trouble Codes', 'In-depth check', 'Live Data'],
+      title: "Common Functions",
+      data: ["Trouble Codes", "In-depth check", "Live Data"],
     },
     {
-      title: 'Diagnostic Test',
-      data: ['Smog Check', 'Battery Check'],
+      title: "Diagnostic Test",
+      data: ["Smog Check", "Battery Check"],
     },
     {
-      title: 'DIY',
-      data: ['Custom Dash', 'Trip Management'],
+      title: "DIY",
+      data: ["Custom Dash", "Trip Management"],
     },
     {
-      title: 'INFO',
-      data: ['Owner\'s Manual'],
-    }
+      title: "INFO",
+      data: ["Owner's Manual"],
+    },
   ];
 
   // Function to handle navigation based on button pressed
   const whichScreen = (name: string) => {
-    switch(name) {
-    case 'Trouble Codes':
-    console.log(`Navigating to Trouble Codes Screen`);
-    break;
-    case 'In-depth check':
-    console.log(`Navigating to In-depth Check Screen`);
-    break;
-    case 'Live Data':
-    console.log(`Navigating to Live Data Screen`);
-    break;
-    case 'Smog Check':
-    console.log(`Navigating to Smog Check Screen`);
-    break;
-    case 'Battery Check':
-    console.log(`Navigating to Battery Check Screen`);
-    break;
-    case 'Custom Dash':
-    console.log(`Navigating to Custom Dash Screen`);
-    break;
-    case 'Trip Management':
-    console.log(`Navigating to Trip Management Screen`);
-    break;
-    case 'Owner\'s Manual':
-      navigate('/(tabs)/(app_center_Routes)/PDFviewer');
-    
-    break;
-    
-    default:
-    console.log(`No screen found`);
-    break;
+    switch (name) {
+      case "Trouble Codes":
+        console.log(`Navigating to Trouble Codes Screen`);
+        break;
+      case "In-depth check":
+        console.log(`Navigating to In-depth Check Screen`);
+        break;
+      case "Live Data":
+        console.log(`Navigating to Live Data Screen`);
+        break;
+      case "Smog Check":
+        console.log(`Navigating to Smog Check Screen`);
+        break;
+      case "Battery Check":
+        console.log(`Navigating to Battery Check Screen`);
+        break;
+      case "Custom Dash":
+        console.log(`Navigating to Custom Dash Screen`);
+        break;
+      case "Trip Management":
+        console.log(`Navigating to Trip Management Screen`);
+        break;
 
+      case "Owner's Manual":
+      router.push("/PDFviewer");
+
+      
+        break;
+
+      default:
+        console.log(`No screen found`);
+
+        break;
     }
   };
-
-
 
   return (
     <ThemedView style={styles.container}>
@@ -93,17 +98,17 @@ const styles = StyleSheet.create({
     padding: 12,
   },
   scrollView: {
-    backgroundColor: 'rgba(50,50,50,1)',
+    backgroundColor: "rgba(50,50,50,1)",
   },
   text: {
     fontSize: 42,
     padding: 12,
-    color: 'rgba(255, 255, 255, 1)'
+    color: "rgba(255, 255, 255, 1)",
   },
   roundedView: {
     flex: 1,
-    backgroundColor: 'rgba(0, 171, 193, 1)',
+    backgroundColor: "rgba(0, 171, 193, 1)",
     borderRadius: 10,
     padding: 5,
-  }
+  },
 });
