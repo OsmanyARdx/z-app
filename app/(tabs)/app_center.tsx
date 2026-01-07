c4c1c36a86320fbfde4e3f62ee1d3f5a54c3fb2a
JPR <jonatanp613@gmail.com>
2026-01-06 19:48:01 -0500

    Showing PDFs & Scrollview on index

--- app/(tabs)/app_center.tsx
+++ app/(tabs)/app_center.tsx
@@ -1,7 +1,8 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { navigate } from 'expo-router/build/global-state/routing';
 import React from 'react';
-import { Button, SectionList, StyleSheet, View } from 'react-native';
+import { Button, SectionList, StyleSheet, Switch, View } from 'react-native';
 
 export default function AppCenterScreen() {
 
@@ -23,6 +24,45 @@ export default function AppCenterScreen() {
       data: ['Owner\'s Manual'],
     }
   ];
+
+  // Function to handle navigation based on button pressed
+  const whichScreen = (name: string) => {
+    switch(name) {
+    case 'Trouble Codes':
+    console.log(`Navigating to Trouble Codes Screen`);
+    break;
+    case 'In-depth check':
+    console.log(`Navigating to In-depth Check Screen`);
+    break;
+    case 'Live Data':
+    console.log(`Navigating to Live Data Screen`);
+    break;
+    case 'Smog Check':
+    console.log(`Navigating to Smog Check Screen`);
+    break;
+    case 'Battery Check':
+    console.log(`Navigating to Battery Check Screen`);
+    break;
+    case 'Custom Dash':
+    console.log(`Navigating to Custom Dash Screen`);
+    break;
+    case 'Trip Management':
+    console.log(`Navigating to Trip Management Screen`);
+    break;
+    case 'Owner\'s Manual':
+      navigate('/(tabs)/(app_center_Routes)/PDFviewer');
+    
+    break;
+    
+    default:
+    console.log(`No screen found`);
+    break;
+
+    }
+  };
+
+
+
   return (
     <ThemedView style={styles.container}>
       <View style={styles.container}>
@@ -34,7 +74,7 @@ export default function AppCenterScreen() {
               <Button
                 title={item}
                 color={"rgba(50, 50, 50, 1)"}
-                onPress={() => console.log(`pressed`)}
+                onPress={() => whichScreen(item)}
               />
             </View>
           )}
